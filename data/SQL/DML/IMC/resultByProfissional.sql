b1b226409980d17db5f6ecd15ed24debcf786614
Lucas Giori Cesconetto <lucasgioricesconetto@gmail.com>
2022-12-18 13:11:19 -0300

    fix:  ajustando cpf retornado na query

--- data/SQL/DML/IMC/resultByProfissional.sql
+++ data/SQL/DML/IMC/resultByProfissional.sql
@@ -1,9 +1,9 @@
 select
     c.nome,
-    u.cpf,
+    c.cpf,
     count(i.cliente_id) as qtd
 from public.usuarios u
          inner join public.imc i on i.profissional_id = u.id
          inner join public.usuarios c on c.id = i.cliente_id
 where u.id = :id
-group by c.nome, u.cpf
+group by c.nome, c.cpf
